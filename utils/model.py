1b534f69487f53745f6388ac56aa7caf2a25408f
Calvin Huang <wixx3r@calvins-mbp.mynetworksettings.com>
2022-11-08 22:23:28 -0500

    added ./utils

---
+++ utils/model.py
@@ -0,0 +1,9 @@
+
+from tokenize import String
+from utils.exts import db
+
+class User(db.Model):
+    __tablename__ = "user_db"
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String)
+    password = db.Column(db.String)
