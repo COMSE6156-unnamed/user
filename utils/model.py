366b80fcb7f7fc28e8c0f4aae60fd2a3614087d9
Calvin Huang <wixx3r@calvins-mbp.mynetworksettings.com>
2022-11-09 17:36:26 -0500

    db bug fix

--- utils/model.py
+++ utils/model.py
@@ -1,9 +1,8 @@
 
-from tokenize import String
 from utils.exts import db
 
 class User(db.Model):
-    __tablename__ = "user_db"
+    __tablename__ = "user"
     user_id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String)
     password = db.Column(db.String)
