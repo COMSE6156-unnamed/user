219b82484f93fd291f084f610ed779d1ae749845
Calvin Huang <wixx3r@calvins-mbp.mynetworksettings.com>
2022-11-30 13:57:43 -0500

    check registration and model update

--- utils/model.py
+++ utils/model.py
@@ -5,9 +5,9 @@ class User(db.Model):
     __tablename__ = "user"
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_name = db.Column(db.String)
-    password = db.Column(db.String)
+    user_email = db.Column(db.String)
 
-    def __init__(self, user_id, user_name, password):
+    def __init__(self, user_id, user_name, user_email):
         self.user_id = user_id
         self.user_name = user_name
-        self.password = password
+        self.user_email = user_email
