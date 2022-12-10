d77ae79be63df87a6939aeef9566e1b6c99207d6
nameslessjin <42518630+nameslessjin@users.noreply.github.com>
2022-12-09 22:58:21 -0500

    load env from .env and fix the env var name

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 from sqlalchemy import create_engine
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 # load environment variables from .env
-load_dotenv()
+load_dotenv(find_dotenv())
 
 # DB related
 HOSTNAME = os.environ.get("RDS_HOSTNAME")
@@ -11,7 +11,7 @@ PORT = os.environ.get("RDS_PORT")
 USERNAME = os.environ.get("DBUSER")
 PASSWORD = os.environ.get("DBPW")
 DATABASE = 'user_db'
-SECRET_KEY = os.environ.get("SECRETKEY_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
